57ee5afd3bbeeca45373d4f90fe9128c873c33dd
Ghani Mudzakir <131063698+KunyitAlami@users.noreply.github.com>
2023-10-26 05:53:10 +0800

    Update PRAK301-2310817110011-Ghani_Mudzakir.py

--- PRAK301/PRAK301-2310817110011-Ghani_Mudzakir.py
+++ PRAK301/PRAK301-2310817110011-Ghani_Mudzakir.py
@@ -1,24 +1,10 @@
-a,b,c = map(int, input().split())
+a,b = map(int, input().split())
 
 if (a<b):
-        if (a<c):
-            if (c<b):
-                print(f"{a} {c} {b}")
-            if (c>b):
-                print(f"{a} {b} {c}")
-        if (a>c):
-            if (c<b):
-                print(f"{c} {a} {b}")
+    print(a,b)
     
 elif (a>b):
-        if (a<c):
-            if (c>b):
-                print(f"{b} {a} {c}")
-        if (a>c):
-            if (c<b):
-                print(f"{c} {b} {a}")
-            if (c>b):
-                print(f"{b} {c} {a}")
+    print(b,a)
             
         
 
