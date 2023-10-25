c1a4890ec54249df55e9336e380f41b1f6be8208
Ghani Mudzakir <131063698+KunyitAlami@users.noreply.github.com>
2023-10-26 05:51:32 +0800

    Update PRAK301-2310817110011-Ghani_Mudzakir.c

--- PRAK301/PRAK301-2310817110011-Ghani_Mudzakir.c
+++ PRAK301/PRAK301-2310817110011-Ghani_Mudzakir.c
@@ -2,60 +2,17 @@
 
 int main()
 {
-    //kita membuat variabel untuk memasukan user input
-    int a,b,c;
-    scanf("%d %d %d", &a,&b,&c);
-    // 2 3 1
-    // 1 3 2
-    // 3 1 
+    int a, b;
+    scanf("%d %d", &a, &b);
 
-    if (a<b)
+    if (a < b)
     {
-        if (a<c)
-        {
-            if (c<b)
-            {
-                printf("%d %d %d", a,c,b);
-            }
-            if (c>b)
-            {
-                printf("%d %d %d", a,b,c);
-            }
-        }
-        if (a>c)
-        {
-            if (c<b)
-            {
-                printf("%d %d %d", c,a,b);
-            }
-        }
+        printf("%d %d", a, b);
     }
-    if (a>b) //2 1 3 // 2 3
+    else
     {
-        if (a<c)
-        {
-            if (c>b)
-            {
-                printf("%d %d %d", b,a,c);
-            }
-        }
-
-        if (a>c) //3 1 2 // 3 2 1
-        {
-            if (c<b)
-            {
-                printf("%d %d %d", c,b,a);
-            }
-            if (c>b)
-            {
-                printf("%d %d %d", b,c,a);
-            }
-        }
+        printf("%d %d", b, a);
     }
-    
 
     return 0;
 }
-
-
-
