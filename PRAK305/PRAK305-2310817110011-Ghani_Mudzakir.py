a0997101aeb0b0bae98a091c9b3c53795b313543
Ghani Mudzakir <131063698+KunyitAlami@users.noreply.github.com>
2023-10-25 22:48:34 +0800

    Update PRAK305-2310817110011-Ghani_Mudzakir.py

--- PRAK305/PRAK305-2310817110011-Ghani_Mudzakir.py
+++ PRAK305/PRAK305-2310817110011-Ghani_Mudzakir.py
@@ -1,11 +1,17 @@
 masukan_detik = int(input())
 
-jam = int(masukan_detik/3600)
+hari = int(masukan_detik/86400)
+sisa_detik_habis_hari = int(masukan_detik%86400)
+jam = int(sisa_detik_habis_hari/3600)
 sisa_detik_habis_jam = int(masukan_detik%3600)
 menit = int(sisa_detik_habis_jam/60)
 detik = int(sisa_detik_habis_jam%60)
 
-print(f"{jam:02d}:{menit:02d}:{detik:02d}")
+if hari == 0:
+    print(f"{jam:02d}:{menit:02d}:{detik:02d}")
+
+elif hari != 0:
+    print(f"{hari:2d} hari {jam:02d}:{menit:02d}:{detik:02d}")
 
 
 
