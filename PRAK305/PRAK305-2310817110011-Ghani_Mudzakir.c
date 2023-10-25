ec7d7b4adcf84cd1c0bf10fe5a1f94c83c2719cf
Ghani Mudzakir <131063698+KunyitAlami@users.noreply.github.com>
2023-10-25 22:48:08 +0800

    Update PRAK305-2310817110011-Ghani_Mudzakir.c

--- PRAK305/PRAK305-2310817110011-Ghani_Mudzakir.c
+++ PRAK305/PRAK305-2310817110011-Ghani_Mudzakir.c
@@ -5,12 +5,20 @@ int main()
     int masukan_detik;
     scanf("%d", &masukan_detik);
 
+    int hari = masukan_detik/86400;
+    int sisa_detik_habis_hari = masukan_detik%86400;
     int jam = masukan_detik/3600;
     int habis_jam = masukan_detik%3600;
     int menit = habis_jam/60;
     int detik = habis_jam%60;
 
-    printf("%02d : %02d : %02d\n", jam,menit,detik);
+    if (hari == 0){
+        printf("%02d : %02d : %02d\n", jam,menit,detik);
+    }
+    else if( hari != 0){
+        printf("%d hari %02d : %02d : %02d\n", hari,jam,menit,detik);
+    }
+
     return 0;
 }
 
